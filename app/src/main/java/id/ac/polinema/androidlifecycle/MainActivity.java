b2a51fec264aee5606b1f08f567aa68a3ad6f138
alifalamiin <yoursweetestn@gmail.com>
2019-09-13 13:34:33 +0700

    Commited

--- app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -1,19 +1,19 @@
 package id.ac.polinema.androidlifecycle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-	// TODO: tambahkan callback onStart() di sini
 
     @Override
     protected void onStart() {
@@ -21,16 +21,11 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "App on Start", Toast.LENGTH_SHORT).show();
     }
 
-        // TODO: tambahkan callback onStop() di sini
-
         @Override
         protected void onStop () {
             super.onStop();
             Toast.makeText(this, "App on Stop", Toast.LENGTH_SHORT).show();
         }
-
-
-            // TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
 
             @Override
             protected void onRestart () {
@@ -39,15 +34,15 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            protected void onPause () {
-                super.onPause();
-                Toast.makeText(this, "App on Pause", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
             protected void onResume () {
                 super.onResume();
                 Toast.makeText(this, "App on Resume", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected void onPause () {
+                super.onPause();
+                Toast.makeText(this, "App on Pause", Toast.LENGTH_SHORT).show();
             }
 
             @Override
